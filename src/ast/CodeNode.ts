87d681ac108f69c1d07586d968d6089e7e8c2bd3
Lim Mingjie, Kenneth <me@kenlimmj.com>
2016-10-08 00:18:40 -0400

    Add indented code detector

--- src/ast/CodeNode.ts
+++ src/ast/CodeNode.ts
@@ -1,31 +1,84 @@
 import ANode from './ANode';
 
-export enum BLOCK_TYPE {
-  Indented,
-  Fenced
-}
+export enum BLOCK_TYPE { Indented, Fenced }
 
+/**
+ * A block of code.
+ *
+ * @export
+ * @class CodeNode
+ * @extends {ANode<string>}
+ */
 export default class CodeNode extends ANode<string> {
-  constructor(content: string, private blockType: BLOCK_TYPE, opt_parent = null,
-    opt_children = []) {
-    super(content, opt_parent, opt_children);
+  constructor(content: string, private blockType: BLOCK_TYPE,
+    private infoString?: string, private indentLevel?: number,
+    parent = undefined, opt_children = []) {
+    super(content, parent, opt_children);
   }
 
   /**
    * Returns the type of the block.
-   * @method getBlockType
-   * @return {BLOCK_TYPE} The type of the block.
+   *
+   * @returns {BLOCK_TYPE} The type of the block.
+   *
+   * @memberOf CodeNode
    */
   getBlockType(): BLOCK_TYPE { return this.blockType; }
 
   /**
+   *Returns the info string for the block, if any.
+   *
+   * @returns {(string | undefined)} The info string for the block.
+   *
+   * @memberOf CodeNode
+   */
+  getInfoString(): string | undefined { return this.infoString; }
+
+  /**
+   * Returns the indent level for the block, if any.
+   *
+   * @returns {(number | undefined)} The indent level for the block.
+   *
+   * @memberOf CodeNode
+   */
+  getIndentLevel(): number | undefined { return this.indentLevel; }
+
+  /**
    * Sets the type of the block.
-   * @method setBlockType
-   * @param  {BLOCK_TYPE} b The block type to be set.
-   * @return {this} {@code this}, for chaining.
+   *
+   * @param {BLOCK_TYPE} b The block type to be set.
+   * @returns {this} {@code this}, for chaining.
+   *
+   * @memberOf CodeNode
    */
   setBlockType(b: BLOCK_TYPE): this {
     this.blockType = b;
+    return this;
+  }
+
+  /**
+   * Sets the info string associated with the block.
+   *
+   * @param {string} s The info string to be set.
+   * @returns {this} {@code this}, for chaining.
+   *
+   * @memberOf CodeNode
+   */
+  setInfoString(s: string): this {
+    this.infoString = s;
+    return this;
+  }
+
+  /**
+   * Sets the indent level associated with the block.
+   *
+   * @param {number} i The indent level to be set.
+   * @returns {this} {@code this}, for chaining.
+   *
+   * @memberOf CodeNode
+   */
+  setIndentLevel(i: number): this {
+    this.indentLevel = i;
     return this;
   }
 }
--- src/processors/IndentedCodeDetector.ts
+++ src/processors/IndentedCodeDetector.ts
@@ -3,6 +3,13 @@ import IProcessor from './IProcessor';
 import BlockNode from '../ast/BlockNode';
 import CodeNode, { BLOCK_TYPE } from '../ast/CodeNode';
 
+/**
+ * Detects indented code.
+ *
+ * @export
+ * @class IndentedCodeDetector
+ * @implements {IProcessor<BlockNode, CodeNode>}
+ */
 export default class IndentedCodeDetector implements IProcessor<BlockNode, CodeNode> {
   process(node: BlockNode): CodeNode | null {
     const nodeContent = node.getContent();
